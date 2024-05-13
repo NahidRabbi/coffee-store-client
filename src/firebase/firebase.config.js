14e2a541964679e446f45cb08956cf389cbb94ea
nahid <nahidrabbi045@gmail.com>
2024-05-13 23:44:46 +0600

    added a authProvider and user database

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA9FUzudQ-FaRDZCC9efwZnaDuZncSx7mo",
+  authDomain: "coffee-store-8fc82.firebaseapp.com",
+  projectId: "coffee-store-8fc82",
+  storageBucket: "coffee-store-8fc82.appspot.com",
+  messagingSenderId: "223897233664",
+  appId: "1:223897233664:web:217e7c5dc5f0fd1c642be5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
